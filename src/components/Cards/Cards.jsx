f651b635c171723ee1b0918979367bdfe7da6e5f
Rahul Sharma <rahulsharmabhumca16@gmail.com>
2022-09-05 00:49:08 +0530

    till cards com 28:56

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Card ,CardContent, Typography,Grid} from '@material-ui/core';
+
+const Cards = ({data}) => {
+  console.log('this is the props data',data);
+  return (
+    <div>Cards</div>
+  )
+}
+
+export default Cards
